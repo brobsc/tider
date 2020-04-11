eb6018a6408c25a72639b35effd26972696e4af1
Rob <bruno.robsiq@gmail.com>
2020-04-11 11:26:54 -0300

    Add error checking for jsonp execution

--- src/tider/app/utils.cljs
+++ src/tider/app/utils.cljs
@@ -1,15 +1,25 @@
 (ns tider.app.utils
   (:require [goog.string]
-            ["moment" :as moment]))
+            [cljs.core.async :refer [<! timeout]]
+            ["moment" :as moment])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(defn jsonp [uri callback _]
-  (let [script (js/document.createElement "script")
-        _ (set! js/window.jsonpCb callback)
-        _ (set! script.src (str (reduce str (drop 5 (seq uri)))
-                            "?jsonp=jsonpCb"))
+(def JSONP_TIMEOUT 3000)
+
+(defn jsonp [uri callback err]
+  (let [run? (atom false)
+        checked-fn (fn [res] (do (callback res) (reset! run? true)))
+        script (js/document.createElement "script")
         head (js/document.querySelector "head")
-        _2 (.appendChild head script)]
-   nil))
+        stripped-uri (->> uri seq (drop 5) (apply str))]
+    (do
+      (set! js/window.callback checked-fn)
+      (set! script.src (str stripped-uri "?jsonp=callback"))
+      (.appendChild head script)
+      (go
+        (<! (timeout JSONP_TIMEOUT))
+        (when (not @run?) (err)))
+      (.removeChild head script))))
 
 (def unescape
   (fnil goog.string/unescapeEntities ""))
